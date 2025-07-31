256cd78472a07dbba4f29678c4517389f69706ce
smacknfor10 <smacknfor1@gmail.com>
2025-07-31 16:11:56 +0200

    About Section Accomplished

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './Components/Navbar'
+import Header from './Components/Header'
+import About from './Components/About'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Header />
+      <About />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,66 @@
+import React, { useEffect, useRef, useState } from 'react'
+import { assets } from '../assets/assets'
+
+const Navbar = () => {
+
+    const [isScroll, setIsScroll] = useState(false)
+    const sideMenuRef = useRef()
+
+    const openMenu = ()=> {
+        sideMenuRef.current.style.transform = 'translateX(-16rem)'
+    }
+    const closeMenu = ()=> {
+        sideMenuRef.current.style.transform = 'translateX(16rem)'
+    }
+
+    useEffect(()=>{
+        window.addEventListener('scroll', ()=>{
+            if (scrollY > 50) {
+                setIsScroll(true)
+            }else {
+                setIsScroll(false)
+            }
+        })
+    },[])
+
+    return (
+        <>
+            <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]'>
+                <img src={assets.header_bg_color} alt="backgroung" className='w-full' />
+            </div>
+            <nav className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50 ${isScroll ? "bg-white bg-opacity-50 backdrop-blur-lg shadow-sm" : ""}`}>
+                <a href="#top"><img src={assets.logo} alt="logo" className='w-28 cursor-pointer mr-14'/></a>
+                
+                <ul className={`hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 ${isScroll ? "" : "bg-white shadow-sm bg-opacity-50"}font-ovo`}>
+                    <li><a href="#top">Home</a></li>
+                    <li><a href="#about">About Me</a></li>
+                    <li><a href="#services">Services</a></li>
+                    <li><a href="#work">My Work</a></li>
+                    <li><a href="#contact">Contact Me</a></li>
+                </ul>
+                <div className='flex items-center gap-4'>
+                    <button><img src={assets.moon_icon} alt="moon-icon" className='w-6'/></button>
+                    <a href="#contact" className='hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4'>Contact <img src={assets.arrow_icon} alt="contact" className='w-3' /></a>
+
+                    <button onClick={openMenu} className='block md:hidden ml-3'> <button><img src={assets.menu_black} alt="black-icon" className='w-6'/></button></button>
+                </div>
+            
+                {/*==== Mobile Menu ====*/}
+                <ul ref={sideMenuRef} className='flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500'>
+                    
+                    <div className='absolute right-6 top-6' onClick={closeMenu}>
+                        <img src={assets.close_black} alt="close-icon" className='w-5 cursor-pointer' />
+                    </div>
+
+                    <li><a onClick={closeMenu} href="#top">Home</a></li>
+                    <li><a onClick={closeMenu} href="#about">About Me</a></li>
+                    <li><a onClick={closeMenu} href="#services">Services</a></li>
+                    <li><a onClick={closeMenu} href="#work">My Work</a></li>
+                    <li><a onClick={closeMenu} href="#contact">Contact Me</a></li>
+                </ul>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar
