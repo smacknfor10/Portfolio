52c13f0f1463e5c5cf25b0a689715dbd47c14dfa
smacknfor10 <smacknfor1@gmail.com>
2025-08-01 18:08:35 +0200

    Last Modification of Today

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
                 <div className='w-max flex items-center gap-2 mx-auto'>
                     <img src={assets.mail_icon} alt="logo" className='w-6' />smacknfor1@gmail.com
                 </div>
-            </div>
+            </div> 
 
             <div className='text-center sm:flex items-center justify-between border-t border-gray-400 mx-[10%] mt-12 py-6'>
                 <p>Copyright {new Date().getFullYear()} Smack. All rights reserved.</p>
